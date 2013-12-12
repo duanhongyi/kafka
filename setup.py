2b485c81091db67c85800c2c242a10331ba8fd16
duanhongyi <duanhongyi@sunmap.com>
2013-12-12 16:07:22 +0800

    add README

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-
+import os
 from setuptools import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
 
 setup(
     name="kafka",
+    version="0.8.1",
     install_requires=["distribute", "poolbase"],
     packages=["kafka"],
     author="duanhongyi",
     author_email="duanhongyi@doopai.com",
     url="https://github.com/duanhongyi/kafka",
-    license="Copyright 2012, David Arthur under Apache License, v2.0",
+    license="Copyright 2012, Apache License, v2.0",
     description="Pure Python client for Apache Kafka",
-    long_description="""
-        This module provides low-level protocol support for Apache Kafka as well as
-        high-level consumer and producer classes. Request batching is supported by the
-        protocol as well as broker-aware request routing. Gzip and Snappy compression
-        is also supported for message sets.
-    """
+    long_description=README,
 )
