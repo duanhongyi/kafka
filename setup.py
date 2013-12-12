28326048031c02d4a764a66e4afa29f70e5e12a9
duanhongyi <duanhongyi@sunmap.com>
2013-12-12 15:00:03 +0800

    del tox

--- setup.py
+++ setup.py
@@ -1,41 +1,19 @@
-import os.path
-import sys
 
-from setuptools import setup, Command
-
-
-class Tox(Command):
-    user_options = []
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        import tox
-        sys.exit(tox.cmdline([]))
-
+from setuptools import setup
 
 setup(
-    name="kafka-python",
-    version="0.8.1-1",
-
-    install_requires=["distribute", "tox"],
-    tests_require=["tox"],
-    cmdclass={"test": Tox},
-
+    name="kafka",
+    install_requires=["distribute", "poolbase"],
     packages=["kafka"],
-
-    author="David Arthur",
-    author_email="mumrah@gmail.com",
-    url="https://github.com/mumrah/kafka-python",
+    author="duanhongyi",
+    author_email="duanhongyi@doopai.com",
+    url="https://github.com/duanhongyi/kafka",
     license="Copyright 2012, David Arthur under Apache License, v2.0",
     description="Pure Python client for Apache Kafka",
     long_description="""
-This module provides low-level protocol support for Apache Kafka as well as
-high-level consumer and producer classes. Request batching is supported by the
-protocol as well as broker-aware request routing. Gzip and Snappy compression
-is also supported for message sets.
-"""
+        This module provides low-level protocol support for Apache Kafka as well as
+        high-level consumer and producer classes. Request batching is supported by the
+        protocol as well as broker-aware request routing. Gzip and Snappy compression
+        is also supported for message sets.
+    """
 )
