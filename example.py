9741dbf7d60347fc5d35386eb731c6fea04207ae
duanhongyi <duanhongyi@doopai.com>
2014-01-23 13:52:25 +0800

    autopep8

--- example.py
+++ example.py
@@ -4,14 +4,17 @@ from kafka.client import KafkaClient, FetchRequest, ProduceRequest
 from kafka.consumer import SimpleConsumer
 from kafka.producer import SimpleProducer
 
+
 def produce_example(client):
     producer = SimpleProducer(client, "my-topic")
     producer.send_messages("test")
+
 
 def consume_example(client):
     consumer = SimpleConsumer(client, "test-group", "my-topic")
     for message in consumer:
         print(message)
+
 
 def main():
     client = KafkaClient("localhost", 9092)
