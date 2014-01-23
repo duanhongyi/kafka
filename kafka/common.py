e6b50d5b9106ad2ce6ea9a23acc1b24a5f6f1005
duanhongyi <duanhongyi@doopai.com>
2014-01-23 14:12:34 +0800

    remove ErrorMapping

--- kafka/common.py
+++ kafka/common.py
@@ -46,21 +46,3 @@ PartitionMetadata = namedtuple("PartitionMetadata",
 OffsetAndMessage = namedtuple("OffsetAndMessage", ["offset", "message"])
 Message = namedtuple("Message", ["magic", "attributes", "key", "value"])
 TopicAndPartition = namedtuple("TopicAndPartition", ["topic", "partition"])
-
-
-class ErrorMapping(object):
-    # Many of these are not actually used by the client
-    UNKNOWN = -1
-    NO_ERROR = 0
-    OFFSET_OUT_OF_RANGE = 1
-    INVALID_MESSAGE = 2
-    UNKNOWN_TOPIC_OR_PARTITON = 3
-    INVALID_FETCH_SIZE = 4
-    LEADER_NOT_AVAILABLE = 5
-    NOT_LEADER_FOR_PARTITION = 6
-    REQUEST_TIMED_OUT = 7
-    BROKER_NOT_AVAILABLE = 8
-    REPLICA_NOT_AVAILABLE = 9
-    MESSAGE_SIZE_TO_LARGE = 10
-    STALE_CONTROLLER_EPOCH = 11
-    OFFSET_METADATA_TOO_LARGE = 12
